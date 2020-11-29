08564e0a79ef9ee8280b2d16ef75e530afd39b5b
dhhruv <180320107529.ce.dhruv@gmail.com>
2020-11-29 21:10:51 +0530

    Added Script

---
+++ settings.py
@@ -0,0 +1,8 @@
+# To use the TinyPNG API you must provide your API key from the website https://tinypng.com/developers
+API_KEY = "lmLwkRtky5Mplvy7f9qHHk32v6XhFgt4"
+
+# Directory containing Input Images
+USER_INPUT_PATH = r"F:\python\image-optimizer-master\TI"
+
+# Directory to save the Output Images
+USER_OUTPUT_PATH = r"F:\python\image-optimizer-master\TO"
